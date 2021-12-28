b51382efbf3647f7fafd44bb40c5194f3377e888
Anay Wadhera <awadhera@berkeley.edu>
2021-12-28 05:57:38 +0530

    SystemUI: Update monet according to new SystemUI stub
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: Iee362e462767c9721bf52620e03cfcb725973345
    
    Co-authored-by: Md. Abu Talha <rokusenpai06@gmail.com>

--- packages/SystemUI/src/org/protonaosp/systemui/CustomSystemUIFactory.java
+++ packages/SystemUI/src/org/protonaosp/systemui/CustomSystemUIFactory.java
@@ -1,14 +1,23 @@
 package org.protonaosp.systemui;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 
 import org.protonaosp.systemui.dagger.DaggerGlobalRootComponentProton;
 import org.protonaosp.systemui.dagger.GlobalRootComponentProton;
 
 import com.android.systemui.SystemUIFactory;
 import com.android.systemui.dagger.GlobalRootComponent;
+import com.android.systemui.navigationbar.gestural.BackGestureTfClassifierProvider;
 
 public class CustomSystemUIFactory extends SystemUIFactory {
+    
+    // ML back gesture provider
+    @Override
+    public BackGestureTfClassifierProvider  createBackGestureTfClassifierProvider(AssetManager am, String modelName) {
+        return new CustomBackGestureTfClassifierProvider(am, modelName);
+    }
+        
     @Override
     protected GlobalRootComponent buildGlobalRootComponent(Context context) {
         return DaggerGlobalRootComponentProton.builder()
