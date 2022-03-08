GrapheneOS-Archive/platform_frameworks_base-old

gmscompat: don't make all services foreground
Instead, rely on an external foreground app to bind to GMS, which
allows GMS to start regular services.

core/java/com/android/internal/gmscompat/GmsCompatApp.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.gmscompat;
+
+import android.content.Context;
+import android.content.Intent;
+
+public final class GmsCompatApp {
+    private static final String TAG = "GmsCompat/App";
+
+    public static final String PKG_NAME = "org.grapheneos.gmscompat";
+
+    private GmsCompatApp() {}
+
+    // this service binds to Play Services and Play Store, thereby raising their priority and
+    // allowing them to start their own services
+    static void startPersistentFgService(Context ctx) {
+        Intent i = new Intent(ctx.getPackageName());
+        i.setClassName(PKG_NAME,
+            PKG_NAME + ".PersistentFgService");
+        ctx.startForegroundService(i);
+    }
+}

XenStuff/frameworks_base-proton

core/java/com/android/internal/gmscompat/GmsCompatApp.java
@@ -1,0 +1,37 @@
+/*
+ * Copyright (C) 2022 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.internal.gmscompat;
+
+import android.content.Context;
+import android.content.Intent;
+
+public final class GmsCompatApp {
+    private static final String TAG = "GmsCompat/App";
+
+    public static final String PKG_NAME = "app.grapheneos.gmscompat";
+
+    private GmsCompatApp() {}
+
+    // this service binds to Play Services and Play Store, thereby raising their priority and
+    // allowing them to start their own services
+    static void startPersistentFgService(Context ctx) {
+        Intent i = new Intent(ctx.getPackageName());
+        i.setClassName(PKG_NAME,
+            PKG_NAME + ".PersistentFgService");
+        ctx.startForegroundService(i);
+    }
+}