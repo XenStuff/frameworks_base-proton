faf0a2a25a70c62c780e4d82e23caaa9d0f87e7d
hpnightowl <iamhp2k@gmail.com>
2022-05-22 07:50:06 -0500

    fwb: LockscreenMedia(Blur|ART): adopt to A12
    Signed-off-by: xenxynon <flynryder427@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -1991,7 +1991,7 @@ public class StatusBar extends SystemUI implements
         }
     }
 
-    private CustomSettingsObserver mCustomSettingsObserver = new CustomSettingsObserver(mHandler);
+    private CustomSettingsObserver mCustomSettingsObserver = new CustomSettingsObserver(mMainHandler);
     private class CustomSettingsObserver extends ContentObserver {
 
         CustomSettingsObserver(Handler handler) {
